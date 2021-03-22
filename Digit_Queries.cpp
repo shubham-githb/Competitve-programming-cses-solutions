eca699d77cd8d3e5a119735f3c4bfe35013f2920
Shubham <shubhamaother@gmail.com>
2021-03-22 19:44:05 +0530

    i

---
+++ Digit_Queries.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define push_back pb
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define mp make_pair
+#define ff first
+#define ss second
+using namespace std;
+
---
+++ Two_Sets.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#define lli long long int
+#define li long int
+#define ld long double
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    lli n;
+    cin>>n;
+    
+}
